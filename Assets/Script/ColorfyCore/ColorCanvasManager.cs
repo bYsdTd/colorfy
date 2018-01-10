790d7c687f3a31b55449e6b296e60e2c9d19a4f6
bYsdTd <nickzy@163.com>
2018-01-10 20:41:49 +0800

    优化涂色的算法，增加几种flood fill 的算法

--- Assets/Script/ColorfyCore/ColorCanvasManager.cs
+++ Assets/Script/ColorfyCore/ColorCanvasManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
-public class ColorCanvasManager : MonoBehaviour
+public partial class ColorCanvasManager : MonoBehaviour
 {
     Color[]     color_cache;
     int         width;
@@ -28,6 +28,28 @@ public class ColorCanvasManager : MonoBehaviour
 		Init();
 		
 		LoadColorTemplate("flower");
+
+        int i = 0;
+        TestRecursion(ref i);
+
+        Debug.Log(i);
+    }
+
+    void TestRecursion(ref int i)
+    {
+        int temp = 0;
+        int temp1 = 1;
+        temp += temp1;
+
+        if(i < 10000)
+        {
+            ++i;
+            TestRecursion(ref i);
+        }
+        else
+        {
+            return;
+        }
     }
 
     public void Init()
@@ -96,7 +118,244 @@ public class ColorCanvasManager : MonoBehaviour
     // }
 
     // x, y 为像素的坐标，屏幕点击的坐标由用户输入相关的模块去处理转换成像素的坐标
-	IEnumerator ColorfyRegion(int x, int y, Color front_color)
+	// IEnumerator ColorfyRegion(int x, int y, Color front_color)
+    // {
+    //     Queue<Vector2i>  open_list = new Queue<Vector2i>();
+    //     HashSet<Vector2i> close_list = new HashSet<Vector2i>();
+
+    //     Vector2i[] neighbor = new Vector2i[4];
+
+    //     if(x < 0 || x >= width || y < 0 || y >= height)
+    //     {
+    //         yield break;
+    //     }
+
+    //     open_list.Clear();
+    //     close_list.Clear();
+        
+    //     open_list.Enqueue(new Vector2i(x, y));
+        
+    //     int time_stamp = System.Environment.TickCount;
+
+    //     while(open_list.Count > 0)
+    //     {
+    //         // 处理当前节点
+    //         Vector2i current_node = open_list.Dequeue();
+
+    //         if(close_list.Contains(current_node))
+    //         {
+    //             continue;
+    //         }
+
+    //         close_list.Add(current_node);
+
+    //         int color_index = current_node.x + current_node.y * width;
+
+    //         Color current_color = color_cache[color_index];
+    //         color_cache[color_index] = front_color;
+
+    //         // 周围的加入队列
+    //         neighbor[0] = new Vector2i(current_node.x-1, current_node.y);
+    //         neighbor[1] = new Vector2i(current_node.x, current_node.y+1);
+    //         neighbor[2] = new Vector2i(current_node.x+1, current_node.y);
+    //         neighbor[3] = new Vector2i(current_node.x, current_node.y-1);
+
+    //         for(int i = 0; i < neighbor.Length; ++i)
+    //         {
+    //             Vector2i node = neighbor[i];
+    //             if(!close_list.Contains(node) && !open_list.Contains(node) && !IsBorder(node))
+    //             {
+    //                 open_list.Enqueue(node);
+    //             }
+    //         }
+
+    //         int elapsed = System.Environment.TickCount - time_stamp;
+
+    //         if(elapsed > 1000)
+    //         {
+    //             RefreshUI();
+    //             time_stamp = System.Environment.TickCount;
+    //             yield return new WaitForSeconds(1.0f);
+    //         }
+    //     }
+
+    //     RefreshUI();
+    // }
+
+    void ColorfyRegionFloodFill(Vector2i node, Color target_color, Color replace_color)
+    {
+        if(target_color == replace_color)
+        {
+            return;
+        }
+
+        if(node.x < 0 || node.x >= width || node.y < 0 || node.y >= height)
+        {
+            return;
+        }
+
+        if(color_cache[node.x + node.y * width] == replace_color)
+        {
+            return;
+        }
+
+        if(color_cache[node.x + node.y * width] != target_color)
+        {
+            return;
+        }
+
+        color_cache[node.x + node.y * width] = replace_color;
+
+        ColorfyRegionFloodFill(new Vector2i(node.x-1, node.y), target_color, replace_color);
+        ColorfyRegionFloodFill(new Vector2i(node.x+1, node.y), target_color, replace_color);
+        ColorfyRegionFloodFill(new Vector2i(node.x, node.y+1), target_color, replace_color);
+        ColorfyRegionFloodFill(new Vector2i(node.x, node.y-1), target_color, replace_color);
+        
+    }
+
+    void ColorfyRegionFloodFillUseQueue(Vector2i node, Color target_color, Color replace_color)
+    {
+        if(target_color == replace_color)
+        {
+            return;
+        }
+
+        if(node.x < 0 || node.x >= width || node.y < 0 || node.y >= height)
+        {
+            return;
+        }
+
+        if(color_cache[node.x + node.y * width] == replace_color)
+        {
+            return;
+        }
+
+        if(color_cache[node.x + node.y * width] != target_color)
+        {
+            return;
+        }
+
+        int color_index = node.x + node.y * width;
+
+        Queue<int> todo_node_list = new Queue<int>();
+        todo_node_list.Enqueue(color_index);
+
+        int max = width * height;
+
+        while(todo_node_list.Count > 0)
+        {
+            int current_node = todo_node_list.Dequeue();
+
+            int left = current_node-1;
+            if(left>=0 && color_cache[left]==target_color) 
+            {
+                todo_node_list.Enqueue(left);
+
+                color_cache[left] = replace_color;
+            }
+
+            int right = current_node+1;
+            if(right<max && color_cache[right]==target_color)
+            {
+                todo_node_list.Enqueue(right);   
+
+                color_cache[right] = replace_color;         
+            } 
+
+            int up = current_node+width;
+            if(up<max && color_cache[up]==target_color)
+            {
+                todo_node_list.Enqueue(up);
+
+                color_cache[up] = replace_color;
+            }
+
+            int down = current_node-width;
+            if(down>=0 && color_cache[down]==target_color)
+            {
+                todo_node_list.Enqueue(down);
+
+                color_cache[down] = replace_color;
+            }
+        }
+    }
+
+    void ColorfyRegionFloodFillUseQueueOpt(Vector2i node, Color target_color, Color replace_color)
+    {
+        if(target_color == replace_color)
+        {
+            return;
+        }
+
+        if(node.x < 0 || node.x >= width || node.y < 0 || node.y >= height)
+        {
+            return;
+        }
+
+        if(color_cache[node.x + node.y * width] == replace_color)
+        {
+            return;
+        }
+
+        if(color_cache[node.x + node.y * width] != target_color)
+        {
+            return;
+        }
+
+        Queue<Vector2i> node_list = new Queue<Vector2i>();
+        node_list.Enqueue(node);
+
+        while(node_list.Count > 0)
+        {
+            Vector2i current_node = node_list.Dequeue();
+
+            int node_index = current_node.x + current_node.y * width;
+            int left_x = current_node.x;
+            int left_index = node_index;
+
+            while(left_x >= 0 && color_cache[left_index] == target_color)
+            {
+                --left_x;
+                --left_index;
+            }
+            
+            int right_x = current_node.x;
+            int right_index = node_index;
+
+            while(right_x < width && color_cache[right_index] == target_color)
+            {
+                ++right_x;
+                ++right_index;
+            }
+
+            int current_node_index = left_index+1;
+
+            for(int x_index = left_x+1; x_index < right_x; ++x_index)
+            {
+                color_cache[current_node_index] = replace_color;
+
+                int up_y = current_node.y+1;
+                int up_index = current_node_index+width;
+                if(up_y<height && color_cache[up_index] == target_color)
+                {
+                    node_list.Enqueue(new Vector2i(x_index, up_y));
+                }
+
+                int down_y = current_node.y-1;
+                int down_index = current_node_index-width;
+                if(down_y>=0 && color_cache[down_index] == target_color)
+                {
+                    node_list.Enqueue(new Vector2i(x_index, down_y));
+                }
+
+                ++current_node_index;
+            }
+
+
+        }
+    }
+
+	void ColorfyRegionIm(int x, int y, Color front_color)
     {
         Queue<Vector2i>  open_list = new Queue<Vector2i>();
         HashSet<Vector2i> close_list = new HashSet<Vector2i>();
@@ -105,15 +364,13 @@ public class ColorCanvasManager : MonoBehaviour
 
         if(x < 0 || x >= width || y < 0 || y >= height)
         {
-            yield break;
+            return;
         }
 
         open_list.Clear();
         close_list.Clear();
         
         open_list.Enqueue(new Vector2i(x, y));
-        
-        int time_stamp = System.Environment.TickCount;
 
         while(open_list.Count > 0)
         {
@@ -145,15 +402,6 @@ public class ColorCanvasManager : MonoBehaviour
                 {
                     open_list.Enqueue(node);
                 }
-            }
-
-            int elapsed = System.Environment.TickCount - time_stamp;
-
-            if(elapsed > 1000)
-            {
-                RefreshUI();
-                time_stamp = System.Environment.TickCount;
-                yield return new WaitForSeconds(1.0f);
             }
         }
 
@@ -205,7 +453,14 @@ public class ColorCanvasManager : MonoBehaviour
             int x = (int)((click_pos.x - current_left_bottom.x) * width / canvas_texture.width);
             int y = (int)((click_pos.y - current_left_bottom.y) * height / canvas_texture.height);
 
-            //ColorfyRegion(x, y, Color.red);
+            //ColorfyRegionIm(x, y, Color.red);
+
+            // ColorfyRegionFloodFill(new Vector2i(x, y), color_cache[x + y * width], Color.red);
+
+            //ColorfyRegionFloodFillUseQueue(new Vector2i(x, y), color_cache[x + y * width], Color.red);
+            ColorfyRegionFloodFillUseQueueOpt(new Vector2i(x, y), color_cache[x + y * width], Color.red);
+
+            RefreshUI();
 
             // if(!is_coloring)
             // {
@@ -223,7 +478,7 @@ public class ColorCanvasManager : MonoBehaviour
 
             // }
 
-            StartCoroutine(ColorfyRegion(x, y, Color.red));
+            //StartCoroutine(ColorfyRegion(x, y, Color.red));
         }
     }
 
